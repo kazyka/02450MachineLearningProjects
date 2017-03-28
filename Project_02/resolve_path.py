89340108045fe515973038dc135143e1ae75ec18
tony <duma0311@gmail.com>
2017-03-29 01:15:41 +0200

    Include ANN in project02_classification

--- Project_02/resolve_path.py
+++ Project_02/resolve_path.py
@@ -7,7 +7,7 @@ Data path: ./insuranceCompany_Data/
 import os
 import sys
 import numpy as np
-from sklearn import cross_validation
+import sklearn.preprocessing as skprep
 
 # Add path to course toolbox
 abs_path = os.path.dirname(os.path.abspath(__file__))
@@ -23,20 +23,35 @@ for fname in datafile:
         print(fname+' is missing!', file=sys.stderr)
 sys.stderr.flush()
 
-# Load data from matlab file
+## Load data from matlab file
 X = np.loadtxt(par_path + '/insuranceCompany_Data/ticdata2000.txt')
-y = X[:,-1]
-X = X[:, 0:-1]
-idx = np.where(y==0)[0]
-keepIdx = np.where(y==1)[0]
-chosenIdx = np.random.choice(idx, size=(400,), replace=False)
-totalIdx = np.append(keepIdx,chosenIdx)
-totalIdx.sort()
-X = X[totalIdx,:]
-y = y[totalIdx]
-
 X_eval = np.loadtxt(par_path + '/insuranceCompany_Data/ticeval2000.txt')
 y_eval = np.loadtxt(par_path + '/insuranceCompany_Data/tictgts2000.txt')
+
+y = X[:,-1]
+X = X[:, 0:-1]
+
+## Uncomment to balance class CARAVAN
+# idx = np.where(y==0)[0]
+# keepIdx = np.where(y==1)[0]
+# chosenIdx = np.random.choice(idx, size=(400,), replace=False)
+# totalIdx = np.append(keepIdx,chosenIdx)
+# totalIdx.sort()
+# X = X[totalIdx,:]
+# y = y[totalIdx]
+
+## Uncoment to use one-out-of-k encoding
+one_out_of_k = [0,4]
+for i in X:
+    for j in one_out_of_k:
+        i[j]-=1
+for i in X_eval:
+    for j in one_out_of_k:
+        i[j]-=1
+encoder = skprep.OneHotEncoder(categorical_features=one_out_of_k)
+encoder.fit(X)
+X = encoder.transform(X).toarray()
+X_eval = encoder.transform(X_eval).toarray()
 
 N = X.shape[0]
 M = X.shape[1]
